4bfecddc5d0a77aa01a3b387b519cc00d31f1104
sip <sip@7c53e7cc-98ea-0310-8f1f-a0b24da60408>
2006-11-28 10:47:15 +0000

    Fixed vt argument detection, thanks to Henrik Brix Andersen
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/slim/trunk@114 7c53e7cc-98ea-0310-8f1f-a0b24da60408

--- app.cpp
+++ app.cpp
@@ -560,13 +560,6 @@ int App::StartServer() {
         } else if (pos == 0) {
             server[argc++] = args+pos;
         }
-        if (server[argc-1][0] == 'v' && server[argc-1][1] == 't') {
-            bool ok = false;
-            Cfg::string2int(server[argc-1]+2, &ok);
-            if (ok) {
-                hasVtSet = true;
-            }
-        }
         ++pos;
 
         if (argc+1 >= MAX_XSERVER_ARGS) {
@@ -576,6 +569,17 @@ int App::StartServer() {
             break;
         }
     }
+
+    for (int i=0; i<argc; i++) {
+        if (server[i][0] == 'v' && server[i][1] == 't') {
+            bool ok = false;
+            Cfg::string2int(server[i]+2, &ok);
+            if (ok) {
+                hasVtSet = true;
+            }
+        }
+    }
+
     if (!hasVtSet && daemonmode) {
         server[argc++] = "vt07";
     }
