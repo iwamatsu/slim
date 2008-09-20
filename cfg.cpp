0e235a82bbfe065c2ace5c3d1e5dd4c2d53e9b1b
sip <sip@7c53e7cc-98ea-0310-8f1f-a0b24da60408>
2008-09-20 13:08:56 +0000

    Removed obsolete X11R6 references
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/slim/trunk@158 7c53e7cc-98ea-0310-8f1f-a0b24da60408

--- cfg.cpp
+++ cfg.cpp
@@ -28,19 +28,19 @@ Cfg::Cfg()
     : currentSession(-1)
 {
     // Configuration options
-    options.insert(option("default_path","./:/bin:/usr/bin:/usr/local/bin:/usr/X11R6/bin"));
-    options.insert(option("default_xserver","/usr/X11R6/bin/X"));
+    options.insert(option("default_path","./:/bin:/usr/bin:/usr/local/bin"));
+    options.insert(option("default_xserver","/usr/bin/X"));
     options.insert(option("xserver_arguments",""));
     options.insert(option("numlock",""));
     options.insert(option("daemon",""));
-    options.insert(option("xauth_path","/usr/X11R6/bin/xauth"));
+    options.insert(option("xauth_path","/usr/bin/xauth"));
     options.insert(option("login_cmd","exec /bin/bash -login ~/.xinitrc %session"));
     options.insert(option("halt_cmd","/sbin/shutdown -h now"));
     options.insert(option("reboot_cmd","/sbin/shutdown -r now"));
     options.insert(option("suspend_cmd",""));
     options.insert(option("sessionstart_cmd",""));
     options.insert(option("sessionstop_cmd",""));
-    options.insert(option("console_cmd","/usr/X11R6/bin/xterm -C -fg white -bg black +sb -g %dx%d+%d+%d -fn %dx%d -T ""Console login"" -e /bin/sh -c ""/bin/cat /etc/issue; exec /bin/login"""));
+    options.insert(option("console_cmd","/usr/bin/xterm -C -fg white -bg black +sb -g %dx%d+%d+%d -fn %dx%d -T ""Console login"" -e /bin/sh -c ""/bin/cat /etc/issue; exec /bin/login"""));
     options.insert(option("screenshot_cmd","import -window root /slim.png"));
     options.insert(option("welcome_msg","Welcome to %host"));
     options.insert(option("default_user",""));
